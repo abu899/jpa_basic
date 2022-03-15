ad7bc3faa9deb0b897624cf3c014e7d0f81c02c6
Ahn <abu89@naver.com>
2022-03-15 21:26:06 +0900

    [JPA BASIC] Basic project test
     + JPA basic project test

--- README.md
+++ README.md
@@ -17,3 +17,24 @@
  - Object Relational Mapping(객체 관계 매핑)
  - 객체는 객체대로 설계, 관계형 데이터베이스는 관계형 데이터베이스 대로 설계
  - ORM 프레임워크가 중간에서 매핑
+
+### Database Dialect 
+
+JPA는 특정 데이터베이스에 종속되지 않는다
+
+- 각각의 데이터베이스가 제공하는 SQL 문법과 함수는 다를 수 있다
+  - 가변 문자: MySQL은 VARCHAR, Oracle은 VARCHAR2
+  - 문자열을 자르는 함수: SQL 표준은 SUBSTRING(), Oracle은 SUBSTR()
+  - 페이징: MySQL은 LIMIT , Oracle은 ROWNUM
+- `Dialect`: SQL 표준을 지키지 않는 특정 데이터베이스만의 고유한 기능
+
+## JPA의 구동 방식
+
+<p align="center"><img src="/imgs/1.png" width="80%"></p>
+
+## 주의사항
+
+- `EntityManagerFactory`는 하나만 생성해서 어플리케이션 전체에서 공유한다
+- `EntityManger`는 쓰레드간 공유가 안된다
+  - 사용하고 버려야한다
+- JPA의 모든 데이터는 `Transaction` 안에서 실행된다!! (매우 중요)
---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,55 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            addMember(em);
+//            findMember(em);
+//            removeMember(em);
+            updateMember(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void addMember(EntityManager em) {
+        Member member = new Member();
+        member.setId(2L);
+        member.setName("helloB");
+        em.persist(member);
+    }
+
+    private static void findMember(EntityManager em) {
+        Member findMember = em.find(Member.class, 1L);
+        System.out.println("findMember.getId() = " + findMember.getId());
+        System.out.println("findMember.getName() = " + findMember.getName());
+    }
+
+    private static void removeMember(EntityManager em) {
+        Member findMember = em.find(Member.class, 1L);
+        em.remove(findMember);
+    }
+
+    private static void updateMember(EntityManager em) {
+        Member findMember = em.find(Member.class, 1L);
+        findMember.setName("HelloChangeJPA");
+    }
+
+
+}
